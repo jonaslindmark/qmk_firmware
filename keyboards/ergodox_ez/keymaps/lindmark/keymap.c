41d02998f84ca04462de5767ee897d8267392326
Jonas Lindmark <lindmark@spotify.com>
2018-05-30 14:23:44 +0200

    Small fixes

--- keyboards/ergodox_ez/keymaps/lindmark/keymap.c
+++ keyboards/ergodox_ez/keymaps/lindmark/keymap.c
@@ -5,7 +5,6 @@
 #define BASE 0 // default layer
 #define ARROWS 1 // symbols
 #define MDIA 2 // media keys
-#define M_TMUX 1
 #define M_PYDEB 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -22,32 +21,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |LGUI  | LAlt |AltShf| `/~  | ESC  |                                       | RCtl | RAlt |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        | App  | Home |       | PgUp  |pydeb|
+ *                                        ,-------------.       ,---------------,
+ *                                        | App  | Home |       | PgUp |        |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | End  |       | PgDn |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | ~L1  |       | ~L2  |        |      |
+ *                                 | Space|Backsp|------|       |------| Enter  |  Del |
+ *                                 |      |ace   | ~L1  |       | RGUI |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,     KC_3,          KC_4,   KC_5, KC_LEFT,
-        KC_TAB,         KC_Q,         KC_W,     KC_E,          KC_R,   KC_T, KC_HOME,
-        KC_LCTL,        KC_A,         KC_S,     KC_D,          KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,     KC_C,          KC_V,   KC_B, ALL_T(KC_NO),
-        KC_LGUI,        KC_LALT,      MO(MDIA), KC_GRAVE,      KC_ESC,
-                                                ALT_T(KC_APP), KC_HOME,
-                                                               KC_END,
-                                      KC_SPC,   KC_BSPC,       MO(ARROWS),
+        KC_EQL,         KC_1,         KC_2,          KC_3,          KC_4,   KC_5, KC_LEFT,
+        KC_TAB,         KC_Q,         KC_W,          KC_E,          KC_R,   KC_T, KC_HOME,
+        KC_LCTL,        KC_A,         KC_S,          KC_D,          KC_F,   KC_G,
+        KC_LSFT,        KC_Z,         KC_X,          KC_C,          KC_V,   KC_B, ALL_T(KC_NO),
+        KC_LGUI,        KC_LALT,      LALT(KC_LSFT), KC_GRAVE,      KC_ESC,
+                                                     ALT_T(KC_APP), KC_HOME,
+                                                                    KC_END,
+                                           KC_SPC,   KC_BSPC,       MO(ARROWS),
         // right hand
              KC_RGHT,      KC_6,       KC_7,    KC_8,   KC_9,   KC_0,    KC_MINS,
              KC_END,       KC_Y,       KC_U,    KC_I,   KC_O,   KC_P,    KC_BSLS,
                            KC_H,       KC_J,    KC_K,   KC_L,   KC_SCLN, KC_QUOT,
              MEH_T(KC_NO), KC_N,       KC_M,    KC_COMM,KC_DOT, KC_SLSH, KC_DEL,
-                                       KC_RCTL, KC_RALT,KC_LBRC,KC_RBRC, KC_RCTL,
+                                       KC_RCTL, KC_RALT,KC_LBRC,KC_RBRC, KC_RSFT,
              KC_PGUP,      M(M_PYDEB),
              KC_PGDN,
              KC_RGUI,      KC_ENT,     KC_DEL
@@ -55,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           | F12  |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           | F12  |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      | [{   |  ]}  |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -75,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [ARROWS] = KEYMAP(
        // left hand
-       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F11,
+       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
@@ -143,10 +142,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   // MACRODOWN only works in this function
   switch(id) {
-    case M_TMUX:
-      // return MACRODOWN(D(LCTL),T(T),U(LCTL),END);
     case M_PYDEB:
-      // return MACRODOWN(I(2),T(I),T(M),T(P),T(O),T(R),T(T),T(SPC),T(I),T(P),T(D),T(B),T(SCLN),T(I),T(P),T(D),T(B),T(DOT),T(S),T(E),T(T),D(LSFT),T(MINS),U(LSFT),T(T),T(R),T(A),T(C),T(E),D(LSFT),T(LPRN),T(RPRN),U(LSFT),END);
       break;
   }
   return MACRO_NONE;
