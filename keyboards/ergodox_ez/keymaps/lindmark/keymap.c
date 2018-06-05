e94f0758566ce893985f132252fe8580b781b076
Jonas Lindmark <lindmark@spotify.com>
2018-06-05 13:05:19 +0200

    Fix mouse keys

--- keyboards/ergodox_ez/keymaps/lindmark/keymap.c
+++ keyboards/ergodox_ez/keymaps/lindmark/keymap.c
@@ -16,10 +16,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | Home |           | End  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   "    |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| Hyper|           |  ~   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LGUI  | LAlt |AltShf| `/~  | ESC  |                                       | RCtl | RAlt |   [  |   ]  | ~L1  |
+ *  |  LGUI | LAlt |AltShf| ~L2  | ESC  |                                       | RCtl | RAlt |      |      | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------,
  *                                        | App  | Home |       | PgUp |        |
@@ -33,19 +33,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,          KC_3,          KC_4,   KC_5, KC_LEFT,
-        KC_TAB,         KC_Q,         KC_W,          KC_E,          KC_R,   KC_T, KC_HOME,
-        KC_LCTL,        KC_A,         KC_S,          KC_D,          KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,          KC_C,          KC_V,   KC_B, ALL_T(KC_NO),
-        KC_LGUI,        KC_LALT,      LALT(KC_LSFT), KC_GRAVE,      KC_ESC,
-                                                     ALT_T(KC_APP), KC_HOME,
-                                                                    KC_END,
-                                           KC_SPC,   KC_BSPC,       MO(ARROWS),
+        KC_EQL,          KC_1,         KC_2,          KC_3,          KC_4,   KC_5, KC_LEFT,
+        KC_TAB,          KC_Q,         KC_W,          KC_E,          KC_R,   KC_T, KC_HOME,
+        KC_LCTL,         KC_A,         KC_S,          KC_D,          KC_F,   KC_G,
+        KC_LSFT,         KC_Z,         KC_X,          KC_C,          KC_V,   KC_B, ALL_T(KC_NO),
+        KC_LGUI,         KC_LALT,      LALT(KC_LSFT), MO(MDIA),      KC_ESC,
+                                                      ALT_T(KC_APP), KC_HOME,
+                                                                     KC_END,
+                                       KC_SPC,        KC_BSPC,       MO(ARROWS),
         // right hand
              KC_RGHT,      KC_6,       KC_7,    KC_8,   KC_9,   KC_0,    KC_MINS,
              KC_END,       KC_Y,       KC_U,    KC_I,   KC_O,   KC_P,    KC_BSLS,
                            KC_H,       KC_J,    KC_K,   KC_L,   KC_SCLN, KC_QUOT,
-             MEH_T(KC_NO), KC_N,       KC_M,    KC_COMM,KC_DOT, KC_SLSH, KC_DEL,
+             KC_GRAVE,     KC_N,       KC_M,    KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
                                        KC_RCTL, KC_RALT,KC_LBRC,KC_RBRC, KC_RSFT,
              KC_PGUP,      M(M_PYDEB),
              KC_PGDN,
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
+ *   |      |      |      |      |      |                                       |VolUp |VolDn | Mute |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, RESET,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, KC_BTN2, KC_BTN1
+       KC_TRNS, KC_BTN1, KC_BTN2
 ),
 };
 
