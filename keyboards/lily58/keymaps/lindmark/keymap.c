5dd370b20bc333f4a84eabc76c3ad65aaf7195b2
Jonas Lindmark <lindmark@spotify.com>
2020-08-12 20:08:15 +0200

    Tweak left CTL position and LOWER

--- keyboards/lily58/keymaps/lindmark/keymap.c
+++ keyboards/lily58/keymaps/lindmark/keymap.c
@@ -36,9 +36,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | =    |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
+ * |CTL/Tab|  Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |?     |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | LOWER|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|   `   |    |   ]   |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | ??   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -48,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_EQL,  KC_1, KC_2, KC_3,    KC_4,  KC_5,                           KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-  KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,  KC_T,                           KC_Y,             KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-  KC_LCTL, KC_A, KC_S, KC_D,    KC_F,  KC_G,                           KC_H,             KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z, KC_X, KC_C,    KC_V,  KC_B,          RAISE,  KC_RBRC, KC_N,             KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-                       KC_LALT, LOWER, RGUI_T(KC_ESC),KC_SPC, KC_ENT,  RCTL_T(KC_GRAVE), KC_BSPC, KC_RALT \
+  KC_EQL,         KC_1, KC_2, KC_3,    KC_4,  KC_5,                           KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+  LCTL_T(KC_TAB), KC_Q, KC_W, KC_E,    KC_R,  KC_T,                           KC_Y,             KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+  LOWER,          KC_A, KC_S, KC_D,    KC_F,  KC_G,                           KC_H,             KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,        KC_Z, KC_X, KC_C,    KC_V,  KC_B,          RAISE,  KC_RBRC, KC_N,             KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+                              KC_LALT, LOWER, RGUI_T(KC_ESC),KC_SPC, KC_ENT,  RCTL_T(KC_GRAVE), KC_BSPC, KC_RALT \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
