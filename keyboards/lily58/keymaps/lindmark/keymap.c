925b54973e9a6e32d3a0679b9cbf495a69a199cb
Jonas Lindmark <lindmark@spotify.com>
2024-04-30 13:08:19 +0200

    small keymap tweaks

--- keyboards/lily58/keymaps/lindmark/keymap.c
+++ keyboards/lily58/keymaps/lindmark/keymap.c
@@ -64,8 +64,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,       KC_F8,         KC_F9,   KC_F10,  KC_F11, \
-  _______, _______, _______, _______, _______, _______,                   _______, KC_LBRC,     KC_RBRC,       _______, _______, _______,\
-  _______, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN,     KC_UP,         KC_RIGHT,_______,_______,\
+  _______, _______, KC_MUTE, KC_VOLU, KC_VOLD, _______,                   _______, KC_LBRC,     KC_RBRC,       _______, _______, _______,\
+  _______, _______, KC_MSTP, KC_MPRV, KC_MNXT, KC_MPLY,                   KC_LEFT, KC_DOWN,     KC_UP,         KC_RIGHT,_______,_______,\
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_WH_UP, KC_MS_WH_DOWN, _______, _______, _______, \
                              _______, _______, _______, _______, KC_DEL, KC_BSPC,  _______, _______\
 ),
